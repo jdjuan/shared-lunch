1a7932bcc2bf13ab15ff9190b16141df60fe845e
H4isan <harlengiraldo@gmail.com>
2017-05-31 09:31:32 -0500

    implementacion: api recibe parametros para enviar emails y valida si el token de firebase es correcto

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var auth = require('./auth');
 var middleware = require('./middleware');
+var admin = require("firebase-admin");
 
 // Configuramos Express
 var app = express();
@@ -11,6 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.set('port', 3000);
+// Configuramos Firebase
+const serviceAccount = require("./resources/testeo-319f48560d0d.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://testeo-ee0f2.firebaseio.com"
+});
 //configuramos el mailer 
 var nodemailer = require('nodemailer');
 // Vamos crear e-mails
@@ -26,15 +33,30 @@ var conta = nodemailer.createTransport({
 // Iniciamos las rutas de nuestro servidor/API
 var router = express.Router();
 
+router.post('/validtoken', function (req, res) {
+    console.log(req);
+    admin.auth().verifyIdToken(req.body.uid)
+        .then(function (decodedToken) {
+            var uid = decodedToken.uid;
+            res.json({ message: uid });
+            // ...
+        }).catch(function (error) {
+            res.json({ message: 'upsii' });
+        });
+
+});
 // Rutas de autenticación y login
 router.post('/auth/sendemail', auth.sendemail);
+// Ruta solo accesible si estás autenticado
+router.get('/private', middleware.ensureAuthenticated, function (req, res) {
+    res.json({ message: 'Hi my bro ' });
+});
 
 
 // Ruta solo accesible si estás autenticado
 router.post('/private', middleware.ensureAuthenticated, function (req, res) {
     res.json({ message: 'your email was send! ' });
     conta.sendMail({
-
         from: 'SharedLunch <sharedlunchv3@gmail.com>',
         to: req.body.destemail1 + '<' + req.body.destemail1 + '>,' + req.body.destemail2 + ' <' + req.body.destemail2 + '>',
         subject: req.body.subject,
